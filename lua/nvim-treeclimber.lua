07cedb6fc51b9a247ad01841d3b1a9eecb933fdc
Dylan C Kendal <dylankendal@gmail.com>
2025-09-08 00:36:06 -0400

    Refactor highlight configuration and setup system
    - Add type annotations for treeclimber.Config and highlight options
    - Extract config management functions (set_config, get_config) 
    - Refactor setup_highlights to support boolean, number, and function options
    - Move highlight initialization to plugin loading with lazy setup
    - Clean up autocmd highlight setup to use centralized setup_highlights function

--- lua/nvim-treeclimber.lua
+++ lua/nvim-treeclimber.lua
@@ -1,14 +1,76 @@
 local M = {}
 
----@class treeclimber.Config
+---@alias treeclimber.Config.highlight boolean | integer | function()
 
+---@class treeclimber.Config
+---@field highlight treeclimber.Config.highlight
+---
+---@class treeclimber.PartialConfig
+---@field highlight? treeclimber.Config.highlight
+---
 ---@type treeclimber.Config
+
 local default_config = {
+	highlight = {
+		auto = true
+	}
 }
 
----@param opts treeclimber.Config
-function M.setup(opts)
+vim.g.treeclimber = vim.tbl_deep_extend('force', {}, default_config)
+
+---@param opts treeclimber.PartialConfig
+function M.set_config(opts)
 	vim.g.treeclimber = vim.tbl_deep_extend('force', default_config, opts)
+end
+
+---@return treeclimber.Config
+function M.get_config()
+	return vim.g.treeclimber
+end
+
+---@param opts treeclimber.PartialConfig
+function M.setup(opts)
+	M.set_config(opts)
+end
+
+function M.setup_highlights()
+	local config = M.get_config()
+
+	local opt = config.highlight
+
+	if opt == false then
+		return
+	end
+
+	if opt == true then
+		-- Default blend value
+		opt = 50
+	end
+
+	if type(opt) == "number" then
+		local hi = require("nvim-treeclimber.hi")
+
+		local Normal = hi.get_hl("Normal", { follow = true })
+		assert(not vim.tbl_isempty(Normal), "hi Normal not found")
+		local normal = hi.HSLUVHighlight:new(Normal)
+
+		local Visual = hi.get_hl("Visual", { follow = true })
+		assert(not vim.tbl_isempty(Visual), "hi Visual not found")
+		local visual = hi.HSLUVHighlight:new(Visual)
+
+		local set_hl = vim.api.nvim_set_hl
+		set_hl(0, "TreeClimberSiblingBoundary", { background = visual.bg.mix(normal.bg, opt).hex })
+		set_hl(0, "TreeClimberSibling", { background = visual.bg.mix(normal.bg, opt).hex })
+		set_hl(0, "TreeClimberParent", { background = visual.bg.mix(normal.bg, opt).hex })
+		set_hl(0, "TreeClimberParentStart", { background = visual.bg.mix(normal.bg, opt).hex })
+
+		return
+	end
+
+	if type(opt) == "function" then
+		opt()
+		return
+	end
 end
 
 return M
--- plugin/nvim-treeclimber.lua
+++ plugin/nvim-treeclimber.lua
@@ -1,99 +1,96 @@
+local loaded = false
+
+
+local function api()
+	if not loaded then
+		loaded = true
+		require("nvim-treeclimber").setup_highlights()
+	end
+
+	return require("nvim-treeclimber.api")
+end
+
 -- Keymaps
 vim.keymap.set("n", "<Plug>(treeclimber-show-control-flow)", function()
-	require("nvim-treeclimber.api").show_control_flow()
+	api().show_control_flow()
 end, {})
 
 vim.keymap.set({ "x", "o" }, "<Plug>(treeclimber-select-current-node)", function()
-	require("nvim-treeclimber.api").select_current_node()
+	api().select_current_node()
 end, { desc = "select current node" })
 
 vim.keymap.set({ "x", "o" }, "<Plug>(treeclimber-select-expand)", function()
-	require("nvim-treeclimber.api").select_expand()
+	api().select_expand()
 end, { desc = "select parent node" })
 
 vim.keymap.set({ "n", "x", "o" }, "<Plug>(treeclimber-select-forward-end)", function()
-	require("nvim-treeclimber.api").select_forward_end()
+	api().select_forward_end()
 end, { desc = "select and move to the end of the node, or the end of the next node" })
 
 vim.keymap.set({ "n", "x", "o" }, "<Plug>(treeclimber-select-backward)", function()
-	require("nvim-treeclimber.api").select_backward()
+	api().select_backward()
 end, { desc = "select and move to the begining of the node, or the beginning of the next node" })
 
 vim.keymap.set({ "n", "x", "o" }, "<Plug>(treeclimber-select-siblings-backward)", function()
-	require("nvim-treeclimber.api").select_siblings_backward()
+	api().select_siblings_backward()
 end, {})
 
 vim.keymap.set({ "n", "x", "o" }, "<Plug>(treeclimber-select-siblings-forward)", function()
-	require("nvim-treeclimber.api").select_siblings_forward()
+	api().select_siblings_forward()
 end, {})
 
 vim.keymap.set({ "n", "x", "o" }, "<Plug>(treeclimber-select-top-level)", function()
-	require("nvim-treeclimber.api").select_top_level()
+	api().select_top_level()
 end, { desc = "select the top level node from the current position" })
 
 vim.keymap.set({ "n", "x", "o" }, "<Plug>(treeclimber-select-previous)", function()
-	require("nvim-treeclimber.api").select_backward()
+	api().select_backward()
 end, { desc = "select previous node" })
 
 vim.keymap.set({ "n", "x", "o" }, "<Plug>(treeclimber-select-shrink)", function()
-	require("nvim-treeclimber.api").select_shrink()
+	api().select_shrink()
 end, { desc = "select child node" })
 
 vim.keymap.set({ "n", "x", "o" }, "<Plug>(treeclimber-select-parent)", function()
-	require("nvim-treeclimber.api").select_expand()
+	api().select_expand()
 end, { desc = "select parent node" })
 
 vim.keymap.set({ "n", "x", "o" }, "<Plug>(treeclimber-select-next)", function()
-	require("nvim-treeclimber.api").select_forward()
+	api().select_forward()
 end, { desc = "select the next node" })
 
 vim.keymap.set({ "n", "x", "o" }, "<Plug>(treeclimber-select-grow-forward)", function()
-	require("nvim-treeclimber.api").select_grow_forward()
+	api().select_grow_forward()
 end, { desc = "Add the next node to the selection" })
 
 vim.keymap.set({ "n", "x", "o" }, "<Plug>(treeclimber-select-grow-backward)", function()
-	require("nvim-treeclimber.api").select_grow_backward()
+	api().select_grow_backward()
 end, { desc = "Add the next node to the selection" })
 
 -- User commands
 
 vim.api.nvim_create_user_command("TCDiffThis", function()
-	require("nvim-treeclimber.api").diff_this()
+	api().diff_this()
 end, { force = true, range = true, desc = "" })
 
 vim.api.nvim_create_user_command("TCHighlightExternalDefinitions", function()
-	require("nvim-treeclimber.api").highlight_external_definitions()
+	api().highlight_external_definitions()
 end, { force = true, range = true, desc = "WIP" })
 
 vim.api.nvim_create_user_command("TCShowControlFlow", function()
-	require("nvim-treeclimber.api").show_control_flow()
+	api().show_control_flow()
 end, {
 	force = true,
 	range = true,
 	desc = "Populate the quick fix with all branches required to reach the current node",
 })
 
-
 local group = vim.api.nvim_create_augroup("nvim-treeclimber-colorscheme", { clear = true })
 
-vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+vim.api.nvim_create_autocmd({ "ColorScheme", "VimEnter" }, {
 	group = group,
 	pattern = "*",
 	callback = function()
-		local hi = require("nvim-treeclimber.hi")
-
-		local Normal = hi.get_hl("Normal", { follow = true })
-		assert(not vim.tbl_isempty(Normal), "hi Normal not found")
-		local normal = hi.HSLUVHighlight:new(Normal)
-
-		local Visual = hi.get_hl("Visual", { follow = true })
-		assert(not vim.tbl_isempty(Visual), "hi Visual not found")
-		local visual = hi.HSLUVHighlight:new(Visual)
-
-		vim.api.nvim_set_hl(0, "TreeClimberHighlight", { background = visual.bg.hex })
-		vim.api.nvim_set_hl(0, "TreeClimberSiblingBoundary", { background = visual.bg.mix(normal.bg, 50).hex })
-		vim.api.nvim_set_hl(0, "TreeClimberSibling", { background = visual.bg.mix(normal.bg, 50).hex })
-		vim.api.nvim_set_hl(0, "TreeClimberParent", { background = visual.bg.mix(normal.bg, 50).hex })
-		vim.api.nvim_set_hl(0, "TreeClimberParentStart", { background = visual.bg.mix(normal.bg, 50).hex })
-	end,
+		require("nvim-treeclimber").setup_highlights()
+	end
 })
